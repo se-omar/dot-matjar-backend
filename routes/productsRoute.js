c5430ebc7c1e19f00b153dfed4f47aff56685998
omarayman639 <miroayman639@gmail.com>
2020-11-25 15:12:37 +0200

    product images fix

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -270,9 +270,9 @@ router.post("/api/product", upload2.array("file", 12), async (req, res, next) =>
             discount_amount: req.body.discount_amount,
             availability: req.body.availability,
             product_rating: req.body.product_rating,
-            main_picture: req.files[0] ? req.files[0].path.substr(11) : null,
-            extra_picture1: req.files[1] ? req.files[1].path.substr(11) : null,
-            extra_picture2: req.files[2] ? req.files[2].path.substr(11) : null,
+            main_picture: req.files[0] ? req.files[0].path.substr(10) : null,
+            extra_picture1: req.files[1] ? req.files[1].path.substr(10) : null,
+            extra_picture2: req.files[2] ? req.files[2].path.substr(10) : null,
             currency: req.body.currency,
         })
 
